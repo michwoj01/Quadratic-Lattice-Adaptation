59082550cc9bbab5f017fe03889aa08a43843f26
Adrianna Bodziony <72196988+adabd-pl@users.noreply.github.com>
2024-12-10 13:00:51 +0100

    Update P21 and tests for P10 and P9

--- src/productions/p21.py
+++ src/productions/p21.py
@@ -1,5 +1,5 @@
 from graph import Graph
-from productions.production import Production
+from production import Production
 from node import Node
 from edge import HyperEdge
 
@@ -8,12 +8,12 @@ from edge import HyperEdge
 class P21(Production):
     def get_left_side(self) -> Graph:
         g = Graph()
-        n1 = Node(0.2,0 , "n1")
-        n2 = Node(1, 0, "n2")
-        n3 = Node(1, 1, "n3")
-        n4 = Node(0.2, 1, "n4")
-        n5 = Node(0, 0.5, "n5")
-        n6 = Node(0.6, 0, "n6")
+        n1 = Node(0.2,0 , "n1", hanging=None)
+        n2 = Node(1, 0, "n2", hanging=None)
+        n3 = Node(1, 1, "n3", hanging=None)
+        n4 = Node(0.2, 1, "n4", hanging=None)
+        n5 = Node(0, 0.5, "n5", hanging=None)
+        n6 = Node(0.6, 0, "n6", hanging=None)
 
         for n in [n1, n2, n3, n4, n5, n6]:
             g.add_node(n)
